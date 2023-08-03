79773d65a9285b16c74b01a721c26f8c4a86ec02
Multirious <77918086+Multirious@users.noreply.github.com>
2023-08-03 22:15:40 +0700

    Update install_dotfile_symlinks.sh

--- installer/install_dotfile_symlinks.sh
+++ installer/install_dotfile_symlinks.sh
@@ -1,13 +1,13 @@
 # In $HOME
-ln -s ~/.dotfiles/files/.gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/files/.zshrc ~/.zshrc
-ln -s ~/.dotfiles/files/.zshenv ~/.zshenv
+ln -sv ~/.dotfiles/files/.gitconfig ~/.gitconfig
+ln -sv ~/.dotfiles/files/.zshrc ~/.zshrc
+ln -sv ~/.dotfiles/files/.zshenv ~/.zshenv
 
 # In $XDG_CONFIG_HOME
-[ ! -d ~/.config ] && mkdir --verbose ~/.config
-ln -s ~/.dotfiles/files/.config/helix ~/.config/helix
-ln -s ~/.dotfiles/files/.config/tmux ~/.config/tmux
+[ ! -d ~/.config ] && mkdir -v ~/.config
+ln -sv ~/.dotfiles/files/.config/helix ~/.config/helix
+ln -sv ~/.dotfiles/files/.config/tmux ~/.config/tmux
 
 # Specialized config locations
-[ ! -d ~/.cargo ] && mkdir --verbose ~/.cargo
-ln -s ~/.dotfiles/files/.cargo/cargo.toml ~/.cargo/cargo.toml
+[ ! -d ~/.cargo ] && mkdir -v ~/.cargo
+ln -sv ~/.dotfiles/files/.cargo/cargo.toml ~/.cargo/cargo.toml
