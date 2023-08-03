45ce794eec9ea699d4e021de67660e2fbedba459
Multirious <77918086+Multirious@users.noreply.github.com>
2023-08-03 22:12:40 +0700

    Update install_dotfile_symlinks.sh

--- installer/install_dotfile_symlinks.sh
+++ installer/install_dotfile_symlinks.sh
@@ -4,10 +4,10 @@ ln -s ~/.dotfiles/files/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/files/.zshenv ~/.zshenv
 
 # In $XDG_CONFIG_HOME
-[ & -d ~/.config ] && mkdir --verbose ~/.config
+[ ! -d ~/.config ] && mkdir --verbose ~/.config
 ln -s ~/.dotfiles/files/.config/helix ~/.config/helix
 ln -s ~/.dotfiles/files/.config/tmux ~/.config/tmux
 
 # Specialized config locations
-[ & -d ~/.cargo ] && mkdir --verbose ~/.cargo
+[ ! -d ~/.cargo ] && mkdir --verbose ~/.cargo
 ln -s ~/.dotfiles/files/.cargo/cargo.toml ~/.cargo/cargo.toml
