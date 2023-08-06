d2108c869f100296f3eb07e4793d566e51e78fcd
Multirious <multirious@outlook.com>
2023-08-06 09:17:56 +0700

    update

--- files/.config/zsh/scripts.zsh
+++ files/.config/zsh/scripts.zsh
@@ -1,9 +1,23 @@
 # === [ tmux ]
-# Create a tmux session on a programming project in a directory
+
+# Create a tmux session in current directory.
+# Will created panes and window inteded for programming.
+# If some parameter is passed then that will be the session's name,
+# otherwise use current directory basename.
 function pg_proj() {
-  proj_path=$1
-  session_name=$2
-  session=
-  session="Programming $proj_path"
-  tmux new-session -d -s $session -n "edit n stuff"
+  session="${1:-$(basename $PWD)}"
+
+  tmux new-session -d -s $session -n "Edit n Stuff"
+  [[ $? -ne 0 ]] && return 1
+
+  tmux split-window -h
+  tmux select-pane -t 0
+  tmux send-keys "hx ." Enter
+  tmux new-window -t $session:1 -n "Other"
+  tmux select-window -t $session:1
+  tmux select-window -t $session:0
+  tmux select-pane -t 1
+
+  [[ -v TMUX ]]  && tmux detach
+  tmux a -t $session
 }
--- files/.config/zsh/zplug.zsh
+++ files/.config/zsh/zplug.zsh
@@ -5,8 +5,9 @@ source $XDG_CONFIG_HOME/zplug/init.zsh
 zplug 'zplug/zplug', hook-build:'zplug --self-manage'
 zplug "romkatv/powerlevel10k", as:theme, depth:1
 zplug "zsh-users/zsh-autosuggestions", use:"zsh-autosuggestions.zsh", depth:1
+zplug "jeffreytse/zsh-vi-mode"
 
-# ===============
+# === [ setting up ]
 
 # Install plugins if there are plugins that have not been installed
 if ! zplug check --verbose; then
@@ -18,3 +19,12 @@ fi
 
 # Then, source plugins and add commands to $PATH
 zplug load --verbose
+
+# === [ configurations ]
+
+#    --- [ "jeffreytse/zsh-vi-mode" ]
+function zvm_config() {
+  ZVM_VI_ESCAPE_BINDKEY=kj
+}
+
+source $ZPLUG_REPOS/jeffreytse/zsh-vi-mode/zsh-vi-mode.zsh
