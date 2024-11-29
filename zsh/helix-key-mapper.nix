c651e5c11f266029311a2d13036dd98263c61139
Multirious <multirious@outlook.com>
2024-11-29 21:08:30 +0700

    asdfasdf

--- zsh/helix-key-mapper.nix
+++ zsh/helix-key-mapper.nix
@@ -1,12 +1,16 @@
 { pkgs }:
 let
   mapZshHelixKeys = keyMappings: /*zsh*/''
-    ZH_MODE=normal
-    ZH_EXTENDING=0
-    ZH_SELECTION_LEFT=0
-    ZH_SELECTION_RIGHT=0
-    ZH_HISTORY=("" 0 0)
-    ZH_HISTORY_IDX=0
+    ZHM_MODE=normal
+    ZHM_EXTENDING=0
+    ZHM_SELECTION_LEFT=0
+    ZHM_SELECTION_RIGHT=0
+    ZHM_HISTORY=("" 0 0 0 0 0 0)
+    ZHM_HISTORY_IDX=1
+    ZHM_BEFORE_INSERT_CURSOR=0
+    ZHM_BEFORE_INSERT_SELECTION_LEFT=0
+    ZHM_BEFORE_INSERT_SELECTION_RIGHT=0
+    ZHM_IN_CMD_HISTORY=0
 
     function dbg {
       tmux send -t 2 -l -- "$*"
@@ -15,44 +19,49 @@ let
 
     function __helix_update_mark {
       REGION_ACTIVE=1
-      if (( (ZH_SELECTION_RIGHT - ZH_SELECTION_LEFT) <= 1 )); then
-        MARK=$ZH_SELECTION_LEFT
-      elif (( (CURSOR + 1) == ZH_SELECTION_RIGHT )); then
-        MARK=$ZH_SELECTION_LEFT
-      elif (( CURSOR == ZH_SELECTION_LEFT )); then
-        MARK=$ZH_SELECTION_RIGHT
+      if (( (ZHM_SELECTION_RIGHT - ZHM_SELECTION_LEFT) <= 1 )); then
+        MARK=$ZHM_SELECTION_LEFT
+      elif (( (CURSOR + 1) == ZHM_SELECTION_RIGHT )); then
+        MARK=$ZHM_SELECTION_LEFT
+      elif (( CURSOR == ZHM_SELECTION_LEFT )); then
+        MARK=$ZHM_SELECTION_RIGHT
       fi
     }
 
     function __helix_update_history {
-      if [[ "$ZH_HISTORY[$((ZH_HISTORY_IDX * 3))]" != "$BUFFER" ]]; then
-        if (( ''${#ZH_HISTORY} < ($ZH_HISTORY_IDX + 1) )); then
-          local count=$((''${#ZH_HISTORY} - $ZH_HISTORY_IDX - 1))
+      if [[ "$ZHM_HISTORY[$((ZHM_HISTORY_IDX * 7 - 6))]" != "$1" ]]; then
+        if (( ''${#ZHM_HISTORY} > ($ZHM_HISTORY_IDX * 7) )); then
+          local count=$(((''${#ZHM_HISTORY} - ZHM_HISTORY_IDX * 7) - 1))
           for i in {0..$count}; do
-            shift -p ZH_HISTORY
-            shift -p ZH_HISTORY
-            shift -p ZH_HISTORY
+            shift -p ZHM_HISTORY
           done
         fi
 
-        ZH_HISTORY+=("$BUFFER")
-        ZH_HISTORY+=($ZH_SELECTION_LEFT)
-        ZH_HISTORY+=($ZH_SELECTION_RIGHT)
+        ZHM_HISTORY+=("$1")
+        ZHM_HISTORY+=($2)
+        ZHM_HISTORY+=($3)
+        ZHM_HISTORY+=($4)
+        ZHM_HISTORY+=($5)
+        ZHM_HISTORY+=($6)
+        ZHM_HISTORY+=($7)
+        ZHM_HISTORY_IDX=$((ZHM_HISTORY_IDX + 1))
+
+        ZHM_IN_CMD_HISTORY=0
       fi
     }
 
     function helix-move_right {
       local prev_cursor=$CURSOR
       CURSOR=$((CURSOR + 1))
-      if (( (prev_cursor + 1) == ZH_SELECTION_RIGHT )); then
-        if (( ZH_EXTENDING != 1 )); then
-          ZH_SELECTION_LEFT=$ZH_SELECTION_RIGHT
+      if (( (prev_cursor + 1) == ZHM_SELECTION_RIGHT )); then
+        if (( ZHM_EXTENDING != 1 )); then
+          ZHM_SELECTION_LEFT=$ZHM_SELECTION_RIGHT
         fi
-        ZH_SELECTION_RIGHT=$((CURSOR + 1))
-      elif (( prev_cursor == ZH_SELECTION_LEFT )); then
-        ZH_SELECTION_LEFT=$CURSOR
-        if (( ZH_EXTENDING != 1 )); then
-          ZH_SELECTION_RIGHT=$((CURSOR + 1))
+        ZHM_SELECTION_RIGHT=$((CURSOR + 1))
+      elif (( prev_cursor == ZHM_SELECTION_LEFT )); then
+        ZHM_SELECTION_LEFT=$CURSOR
+        if (( ZHM_EXTENDING != 1 )); then
+          ZHM_SELECTION_RIGHT=$((CURSOR + 1))
         fi
       fi
 
@@ -62,18 +71,46 @@ let
     function helix-move_left {
       local prev_cursor=$CURSOR
       CURSOR=$((CURSOR - 1))
-      if (( prev_cursor == ZH_SELECTION_LEFT )); then
-        if (( ZH_EXTENDING != 1 )); then
-          ZH_SELECTION_RIGHT=$ZH_SELECTION_LEFT
+      if (( prev_cursor == ZHM_SELECTION_LEFT )); then
+        if (( ZHM_EXTENDING != 1 )); then
+          ZHM_SELECTION_RIGHT=$ZHM_SELECTION_LEFT
         fi
-        ZH_SELECTION_LEFT=$CURSOR
-      elif (( (prev_cursor + 1) == ZH_SELECTION_RIGHT )); then
-        ZH_SELECTION_RIGHT=$((CURSOR + 1))
-        if (( ZH_EXTENDING != 1 )); then
-          ZH_SELECTION_LEFT=$CURSOR
+        ZHM_SELECTION_LEFT=$CURSOR
+      elif (( (prev_cursor + 1) == ZHM_SELECTION_RIGHT )); then
+        ZHM_SELECTION_RIGHT=$((CURSOR + 1))
+        if (( ZHM_EXTENDING != 1 )); then
+          ZHM_SELECTION_LEFT=$CURSOR
         fi
       fi
 
+      __helix_update_mark
+    }
+
+    function helix-move_up {
+      helix-normal
+      ZHM_EXTENDING=0
+      ZHM_SELECTION_LEFT=0
+      ZHM_SELECTION_RIGHT=0
+      local prev_histno=$HISTNO
+      HISTNO=$((HISTNO - 1))
+      dbg "$prev_histno -> $HISTNO max: $HISTCMD "
+      ZHM_SELECTION_LEFT=$CURSOR
+      ZHM_SELECTION_RIGHT=$(($CURSOR + 1))
+      ZHM_SELECTION_RIGHT=$((ZHM_SELECTION_RIGHT < ''${#BUFFER} ? ZHM_SELECTION_RIGHT : ''${#BUFFER}))
+      __helix_update_mark
+    }
+
+    function helix-move_down {
+      helix-normal
+      ZHM_EXTENDING=0
+      ZHM_SELECTION_LEFT=0
+      ZHM_SELECTION_RIGHT=0
+      local prev_histno=$HISTNO
+      HISTNO=$((HISTNO + 1))
+      dbg "$prev_histno -> $HISTNO max: $HISTCMD "
+      ZHM_SELECTION_LEFT=$CURSOR
+      ZHM_SELECTION_RIGHT=$(($CURSOR + 1))
+      ZHM_SELECTION_RIGHT=$((ZHM_SELECTION_RIGHT < ''${#BUFFER} ? ZHM_SELECTION_RIGHT : ''${#BUFFER}))
       __helix_update_mark
     }
 
@@ -95,24 +132,24 @@ let
           fi
         fi
 
-        if (( prev_cursor == ZH_SELECTION_LEFT )); then
-          if (( ZH_EXTENDING != 1 )); then
-            ZH_SELECTION_LEFT=$((prev_cursor + skip))
-            ZH_SELECTION_RIGHT=$((CURSOR + 1))
+        if (( prev_cursor == ZHM_SELECTION_LEFT )); then
+          if (( ZHM_EXTENDING != 1 )); then
+            ZHM_SELECTION_LEFT=$((prev_cursor + skip))
+            ZHM_SELECTION_RIGHT=$((CURSOR + 1))
           else
-            if (( CURSOR >= ZH_SELECTION_RIGHT )); then
-              ZH_SELECTION_LEFT=$((ZH_SELECTION_RIGHT - 1))
-              ZH_SELECTION_RIGHT=$((CURSOR + 1))
+            if (( CURSOR >= ZHM_SELECTION_RIGHT )); then
+              ZHM_SELECTION_LEFT=$((ZHM_SELECTION_RIGHT - 1))
+              ZHM_SELECTION_RIGHT=$((CURSOR + 1))
             else
-              ZH_SELECTION_LEFT=$CURSOR
+              ZHM_SELECTION_LEFT=$CURSOR
             fi
           fi
-        elif (( (prev_cursor + 1) == ZH_SELECTION_RIGHT )); then
-          if (( ZH_EXTENDING != 1 )); then
-            ZH_SELECTION_RIGHT=$((CURSOR + 1))
-            ZH_SELECTION_LEFT=$((prev_cursor + skip))
+        elif (( (prev_cursor + 1) == ZHM_SELECTION_RIGHT )); then
+          if (( ZHM_EXTENDING != 1 )); then
+            ZHM_SELECTION_RIGHT=$((CURSOR + 1))
+            ZHM_SELECTION_LEFT=$((prev_cursor + skip))
           else
-            ZH_SELECTION_RIGHT=$((CURSOR + 1))
+            ZHM_SELECTION_RIGHT=$((CURSOR + 1))
           fi
         fi
       fi
@@ -121,7 +158,6 @@ let
     }
 
     function helix-move_prev_word_start {
-      # 
       local rev_buffer="$(echo "$BUFFER" | rev)"
       local prev_cursor=$CURSOR
       local substring="''${rev_buffer:$((-CURSOR - 1))}"
@@ -140,22 +176,22 @@ let
           fi
         fi
 
-        if (( (prev_cursor + 1) == ZH_SELECTION_RIGHT )); then
-          if (( ZH_EXTENDING != 1 )); then
-            ZH_SELECTION_RIGHT=$((prev_cursor - skip + 1))
-            ZH_SELECTION_LEFT=$CURSOR
+        if (( (prev_cursor + 1) == ZHM_SELECTION_RIGHT )); then
+          if (( ZHM_EXTENDING != 1 )); then
+            ZHM_SELECTION_RIGHT=$((prev_cursor - skip + 1))
+            ZHM_SELECTION_LEFT=$CURSOR
           else
-            if (( CURSOR < ZH_SELECTION_LEFT )); then
-              ZH_SELECTION_RIGHT=$((ZH_SELECTION_LEFT + 1))
-              ZH_SELECTION_LEFT=$CURSOR
+            if (( CURSOR < ZHM_SELECTION_LEFT )); then
+              ZHM_SELECTION_RIGHT=$((ZHM_SELECTION_LEFT + 1))
+              ZHM_SELECTION_LEFT=$CURSOR
             else
-              ZH_SELECTION_RIGHT=$((CURSOR + 1))
+              ZHM_SELECTION_RIGHT=$((CURSOR + 1))
             fi
           fi
-        elif (( prev_cursor == ZH_SELECTION_LEFT )); then
-          ZH_SELECTION_LEFT=$CURSOR
-          if (( ZH_EXTENDING != 1 )); then
-            ZH_SELECTION_RIGHT=$((prev_cursor))
+        elif (( prev_cursor == ZHM_SELECTION_LEFT )); then
+          ZHM_SELECTION_LEFT=$CURSOR
+          if (( ZHM_EXTENDING != 1 )); then
+            ZHM_SELECTION_RIGHT=$((prev_cursor))
           fi
         fi
       fi
@@ -180,24 +216,24 @@ let
           fi
         fi
 
-        if (( prev_cursor == ZH_SELECTION_LEFT )); then
-          if (( ZH_EXTENDING != 1 )); then
-            ZH_SELECTION_LEFT=$((prev_cursor + skip))
-            ZH_SELECTION_RIGHT=$((CURSOR + 1))
+        if (( prev_cursor == ZHM_SELECTION_LEFT )); then
+          if (( ZHM_EXTENDING != 1 )); then
+            ZHM_SELECTION_LEFT=$((prev_cursor + skip))
+            ZHM_SELECTION_RIGHT=$((CURSOR + 1))
           else
-            if (( CURSOR >= ZH_SELECTION_RIGHT )); then
-              ZH_SELECTION_LEFT=$((ZH_SELECTION_RIGHT - 1))
-              ZH_SELECTION_RIGHT=$((CURSOR + 1))
+            if (( CURSOR >= ZHM_SELECTION_RIGHT )); then
+              ZHM_SELECTION_LEFT=$((ZHM_SELECTION_RIGHT - 1))
+              ZHM_SELECTION_RIGHT=$((CURSOR + 1))
             else
-              ZH_SELECTION_LEFT=$CURSOR
+              ZHM_SELECTION_LEFT=$CURSOR
             fi
           fi
-        elif (( (prev_cursor + 1) == ZH_SELECTION_RIGHT )); then
-          if (( ZH_EXTENDING != 1 )); then
-            ZH_SELECTION_RIGHT=$((CURSOR + 1))
-            ZH_SELECTION_LEFT=$((prev_cursor + skip))
+        elif (( (prev_cursor + 1) == ZHM_SELECTION_RIGHT )); then
+          if (( ZHM_EXTENDING != 1 )); then
+            ZHM_SELECTION_RIGHT=$((CURSOR + 1))
+            ZHM_SELECTION_LEFT=$((prev_cursor + skip))
           else
-            ZH_SELECTION_RIGHT=$((CURSOR + 1))
+            ZHM_SELECTION_RIGHT=$((CURSOR + 1))
           fi
         fi
       fi
@@ -206,17 +242,23 @@ let
     }
 
     function helix-insert {
+      ZHM_BEFORE_INSERT_CURSOR=$CURSOR
+      ZHM_BEFORE_INSERT_SELECTION_LEFT=$ZHM_SELECTION_LEFT
+      ZHM_BEFORE_INSERT_SELECTION_RIGHT=$ZHM_SELECTION_RIGHT
       bindkey -A hins main
-      ZH_MODE=insert
-      CURSOR=$ZH_SELECTION_LEFT
+      ZHM_MODE=insert
+      CURSOR=$ZHM_SELECTION_LEFT
       echo -ne '\e[5 q'
       __helix_update_mark
     }
 
     function helix-append {
+      ZHM_BEFORE_INSERT_CURSOR=$CURSOR
+      ZHM_BEFORE_INSERT_SELECTION_LEFT=$ZHM_SELECTION_LEFT
+      ZHM_BEFORE_INSERT_SELECTION_RIGHT=$ZHM_SELECTION_RIGHT
       bindkey -A hins main
-      ZH_MODE=insert
-      CURSOR=$ZH_SELECTION_RIGHT
+      ZHM_MODE=insert
+      CURSOR=$ZHM_SELECTION_RIGHT
       echo -ne '\e[5 q'
       CURSOR=$((CURSOR - 1))
       __helix_update_mark
@@ -224,33 +266,33 @@ let
     }
 
     function helix-normal {
-      if [[ $ZH_MODE == insert ]]; then
-        if [[ $CURSOR == $ZH_SELECTION_RIGHT ]]; then
-          if (( ZH_SELECTION_LEFT < ZH_SELECTION_RIGHT  )); then
+      if [[ $ZHM_MODE == insert ]]; then
+        if [[ $CURSOR == $ZHM_SELECTION_RIGHT ]]; then
+          if (( ZHM_SELECTION_LEFT < ZHM_SELECTION_RIGHT  )); then
             CURSOR=$((CURSOR - 1))
           else
-            ZH_SELECTION_RIGHT=$((ZH_SELECTION_RIGHT + 1))
+            ZHM_SELECTION_RIGHT=$((ZHM_SELECTION_RIGHT + 1))
             local buffer_len=''${#BUFFER}
-            ZH_SELECTION_RIGHT=$((ZH_SELECTION_RIGHT < buffer_len ? ZH_SELECTION_RIGHT : buffer_len))
+            ZHM_SELECTION_RIGHT=$((ZHM_SELECTION_RIGHT < buffer_len ? ZHM_SELECTION_RIGHT : buffer_len))
           fi
         fi
 
-        __helix_update_history
+        __helix_update_history "$BUFFER" $ZHM_BEFORE_INSERT_CURSOR $ZHM_BEFORE_INSERT_SELECTION_LEFT $ZHM_BEFORE_INSERT_SELECTION_RIGHT $CURSOR $ZHM_SELECTION_LEFT $ZHM_SELECTION_RIGHT
       fi
       bindkey -A hnor main
-      ZH_MODE=normal
-      ZH_EXTENDING=0
+      ZHM_MODE=normal
+      ZHM_EXTENDING=0
       echo -ne '\e[2 q'
       __helix_update_mark
     }
 
     function helix-select {
       bindkey -A hnor main
-      ZH_MODE=normal
-      if ((ZH_EXTENDING == 1)); then
-        ZH_EXTENDING=0
+      ZHM_MODE=normal
+      if ((ZHM_EXTENDING == 1)); then
+        ZHM_EXTENDING=0
       else
-        ZH_EXTENDING=1
+        ZHM_EXTENDING=1
       fi
       echo -ne '\e[2 q'
       __helix_update_mark
@@ -260,29 +302,26 @@ let
       local prev_cursor=$CURSOR
       zle .self-insert
 
-      if (( prev_cursor == ZH_SELECTION_LEFT )); then
-        ZH_SELECTION_LEFT=$((ZH_SELECTION_LEFT + 1))
-        ZH_SELECTION_RIGHT=$((ZH_SELECTION_RIGHT + 1))
-      else
-        ZH_SELECTION_RIGHT=$((ZH_SELECTION_RIGHT + 1))
+      if (( prev_cursor == ZHM_SELECTION_LEFT )); then
+        ZHM_SELECTION_LEFT=$((ZHM_SELECTION_LEFT + 1))
       fi
+      ZHM_SELECTION_RIGHT=$((ZHM_SELECTION_RIGHT + 1))
 
       __helix_update_mark
     }
 
     function helix-delete_char_backward {
-      local prev_cursor=$CURSOR
       zle backward-delete-char
 
-      if (( prev_cursor == ZH_SELECTION_LEFT )); then
-        ZH_SELECTION_LEFT=$(($ZH_SELECTION_LEFT - 1))
-        ZH_SELECTION_RIGHT=$(($ZH_SELECTION_RIGHT - 1))
-        ZH_SELECTION_LEFT=$((ZH_SELECTION_LEFT > 0 ? ZH_SELECTION_LEFT : 0))
-        ZH_SELECTION_RIGHT=$((ZH_SELECTION_RIGHT > 0 ? ZH_SELECTION_RIGHT : 0))
+      if (( prev_cursor == ZHM_SELECTION_LEFT )); then
+        ZHM_SELECTION_LEFT=$(($ZHM_SELECTION_LEFT - 1))
+        ZHM_SELECTION_RIGHT=$(($ZHM_SELECTION_RIGHT - 1))
+        ZHM_SELECTION_LEFT=$((ZHM_SELECTION_LEFT > 0 ? ZHM_SELECTION_LEFT : 0))
+        ZHM_SELECTION_RIGHT=$((ZHM_SELECTION_RIGHT > 0 ? ZHM_SELECTION_RIGHT : 0))
       else
-        ZH_SELECTION_RIGHT=$(($ZH_SELECTION_RIGHT - 1))
-        if (( ZH_SELECTION_RIGHT < ZH_SELECTION_LEFT )); then
-          ZH_SELECTION_LEFT=$ZH_SELECTION_RIGHT
+        ZHM_SELECTION_RIGHT=$(($ZHM_SELECTION_RIGHT - 1))
+        if (( ZHM_SELECTION_RIGHT < ZHM_SELECTION_LEFT )); then
+          ZHM_SELECTION_LEFT=$ZHM_SELECTION_RIGHT
         fi
       fi
 
@@ -290,50 +329,60 @@ let
     }
 
     function helix-delete {
-      BUFFER="''${BUFFER:0:$ZH_SELECTION_LEFT}''${BUFFER:$ZH_SELECTION_RIGHT}"
-      __helix_update_history
-      ZH_SELECTION_RIGHT=$((ZH_SELECTION_LEFT + 1))
-      local buffer_len=''${#BUFFER}
-      ZH_SELECTION_RIGHT=$((ZH_SELECTION_RIGHT < buffer_len ? ZH_SELECTION_RIGHT : buffer_len))
-      ZH_EXTENDING=0
+      local prev_cursor=$CURSOR
+      local prev_left=$ZHM_SELECTION_LEFT
+      local prev_right=$ZHM_SELECTION_RIGHT
 
+      BUFFER="''${BUFFER:0:$ZHM_SELECTION_LEFT}''${BUFFER:$ZHM_SELECTION_RIGHT}"
+      ZHM_SELECTION_RIGHT=$((ZHM_SELECTION_LEFT + 1))
+      local buffer_len=''${#BUFFER}
+      ZHM_SELECTION_RIGHT=$((ZHM_SELECTION_RIGHT < buffer_len ? ZHM_SELECTION_RIGHT : buffer_len))
+      CURSOR=$ZHM_SELECTION_LEFT
+
+      ZHM_EXTENDING=0
+
+      __helix_update_history "$BUFFER" $prev_cursor $prev_left $prev_right $CURSOR $ZHM_SELECTION_LEFT $ZHM_SELECTION_RIGHT
       __helix_update_mark
     }
 
     function helix-undo {
-      if ((ZSH_HISTORY_IDX > 0)); then
-        ZSH_HISTORY_IDX=$((ZSH_HISTORY_IDX - 1))
-        BUFFER="$ZSH_HISTORY[$ZSH_HISTORY_IDX]"
-        ZH_SELECTION_LEFT="$ZSH_HISTORY[$((ZSH_HISTORY_IDX + 1))]"
-        ZH_SELECTION_RIGHT="$ZSH_HISTORY[$((ZSH_HISTORY_IDX + 2))]"
+      if ((ZHM_HISTORY_IDX > 1)); then
+        ZHM_HISTORY_IDX=$((ZHM_HISTORY_IDX - 1))
+        BUFFER="$ZHM_HISTORY[$(($ZHM_HISTORY_IDX * 7 - 6))]"
+        CURSOR="$ZHM_HISTORY[$(((ZHM_HISTORY_IDX + 1) * 7 - 5))]"
+        ZHM_SELECTION_LEFT="$ZHM_HISTORY[$(((ZHM_HISTORY_IDX + 1) * 7 - 4))]"
+        ZHM_SELECTION_RIGHT="$ZHM_HISTORY[$(((ZHM_HISTORY_IDX + 1) * 7 - 3))]"
         __helix_update_mark
       fi
     }
 
     function helix-redo {
-      if ((ZSH_HISTORY_IDX < (''${#ZH_HISTORY} - 1))); then
-        ZSH_HISTORY_IDX=$((ZSH_HISTORY_IDX - 1))
-        BUFFER="$ZSH_HISTORY[$ZSH_HISTORY_IDX]"
-        ZH_SELECTION_LEFT="$ZSH_HISTORY[$((ZSH_HISTORY_IDX + 1))]"
-        ZH_SELECTION_RIGHT="$ZSH_HISTORY[$((ZSH_HISTORY_IDX + 2))]"
+      if (((ZHM_HISTORY_IDX * 7) < ''${#ZHM_HISTORY})); then
+        ZHM_HISTORY_IDX=$((ZHM_HISTORY_IDX + 1))
+        BUFFER="$ZHM_HISTORY[$(($ZHM_HISTORY_IDX * 7 - 6))]"
+        CURSOR="$ZHM_HISTORY[$((ZHM_HISTORY_IDX * 7 - 2))]"
+        ZHM_SELECTION_LEFT="$ZHM_HISTORY[$((ZHM_HISTORY_IDX * 7 - 1))]"
+        ZHM_SELECTION_RIGHT="$ZHM_HISTORY[$((ZHM_HISTORY_IDX * 7))]"
         __helix_update_mark
       fi
     }
 
     function helix-accept {
       helix-normal
-      ZH_EXTENDING=0
-      ZH_SELECTION_LEFT=0
-      ZH_SELECTION_RIGHT=0
+      ZHM_EXTENDING=0
+      ZHM_SELECTION_LEFT=0
+      ZHM_SELECTION_RIGHT=0
       zle accept-line
       MARK=
       REGION_ACTIVE=0
-      ZH_HISTORY=("" 0 0)
-      ZH_HISTORY_IDX=0
+      ZHM_HISTORY=("" 0 0 0 0 0 0)
+      ZHM_HISTORY_IDX=1
     }
 
     zle -N helix-move_left
     zle -N helix-move_right
+    zle -N helix-move_down
+    zle -N helix-move_up
     zle -N helix-move_next_word_start
     zle -N helix-move_prev_word_start
     zle -N helix-move_next_word_end
@@ -354,8 +403,8 @@ let
     bindkey -A hnor main
     
     bindkey -M hnor h helix-move_left
-    # bindkey -M hnor j
-    # bindkey -M hnor k
+    bindkey -M hnor j helix-move_down
+    bindkey -M hnor k helix-move_up
     bindkey -M hnor l helix-move_right
     bindkey -M hnor i helix-insert
     bindkey -M hnor a helix-append
