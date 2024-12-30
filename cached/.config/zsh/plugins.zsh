0dde3bc42e1032d14a8b1c99d25a7f27106f195c
Multirious <multirious@outlook.com>
2024-12-30 18:54:33 +0700

    h

--- cached/.config/zsh/plugins.zsh
+++ cached/.config/zsh/plugins.zsh
@@ -1,2 +1,17 @@
-# source /nix/store/20kiw9gxh1b28bwkgykha9f3271k9cxr-source/zsh-helix-mode.plugin.zsh
-# bindkey -M hins "jk" zhm_normal
+source /nix/store/fm2zn0cm6drybl2qkracfis1qipcnfbs-source/zsh-helix-mode.plugin.zsh
+bindkey -M hins "jk" zhm_normal
+
+source /nix/store/lqxgy066krnq2z8pvhqxrj7pcldz1xkg-source/zsh-autosuggestions.plugin.zsh
+ZSH_AUTOSUGGEST_CLEAR_WIDGETS+=(
+  zhm_history_prev
+  zhm_history_next
+)
+ZSH_AUTOSUGGEST_ACCEPT_WIDGETS+=(
+  zhm_move_right
+  zhm_goto_line_end
+)
+ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS+=(
+  zhm_move_next_word_start
+  zhm_move_prev_word_start
+  zhm_move_next_word_end
+)
--- zsh/default.nix
+++ zsh/default.nix
@@ -5,13 +5,34 @@ let
       zsh-helix-mode = fetchFromGitHub {
         owner = "multirious";
         repo = "zsh-helix-mode";
-        rev = "5f3f58b9aff8296eddc3cc9f6a3ead99b9d0bf02";
-        sha256 = "sha256-36V8guzkFQpf3ryX3XnUVtoMHO0OVgm8uAhYasO4uB0=";
+        rev = "165231b3f8677e15b922d21e7cdbe11991c88e0b";
+        sha256 = "sha256-EbXBWwwXJdt6f97+A55M0wIb2mDUDZDEKs2RxAus14c=";
+      };
+      zsh-autosuggestions = fetchFromGitHub {
+        owner = "zsh-users";
+        repo = "zsh-autosuggestions";
+        rev = "0e810e5afa27acbd074398eefbe28d13005dbc15";
+        sha256 = "sha256-85aw9OM2pQPsWklXjuNOzp9El1MsNb+cIiZQVHUzBnk=";
       };
     in
     ''
-      # source ${zsh-helix-mode}/zsh-helix-mode.plugin.zsh
-      # bindkey -M hins "jk" zhm_normal
+      source ${zsh-helix-mode}/zsh-helix-mode.plugin.zsh
+      bindkey -M hins "jk" zhm_normal
+
+      source ${zsh-autosuggestions}/zsh-autosuggestions.plugin.zsh
+      ZSH_AUTOSUGGEST_CLEAR_WIDGETS+=(
+        zhm_history_prev
+        zhm_history_next
+      )
+      ZSH_AUTOSUGGEST_ACCEPT_WIDGETS+=(
+        zhm_move_right
+        zhm_goto_line_end
+      )
+      ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS+=(
+        zhm_move_next_word_start
+        zhm_move_prev_word_start
+        zhm_move_next_word_end
+      )
     '');
 in
 mkConfig {
