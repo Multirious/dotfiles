6d262f7e62523023640f4d0e31a6a48335365730
Multirious <multirious@outlook.com>
2025-01-22 10:08:38 +0700

    ok

--- cached/.config/zsh/completion.zsh
+++ cached/.config/zsh/completion.zsh
@@ -6,5 +6,5 @@ _comp_options+=(globdots) # With hidden files
 zstyle ':completion:*' completer _extensions _complete _approximate
 zstyle ':completion:*' use-cache on
 zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/zcompcache"
-zstyle ':completion:*' menu select search
+zstyle ':completion:*' menu select
 
